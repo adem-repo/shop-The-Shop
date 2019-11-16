e5a862be098454379cbb58cc7f79f2307474437d
HellBjorn <dementyevalexander.m@gmail.com>
2019-11-17 01:22:47 +0300

    something

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import classes from './App.module.scss';
 
 function App() {
+  console.log(classes);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={classes.App}>
+      <header className={classes['App-header']}>
+        <img src={logo} className={classes.AppLogo} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className={classes.AppLink}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
